a3220053934740a089a2d20f494eb43ea5444a7e
mnishigawa <147137466+mnishigawa@users.noreply.github.com>
2023-10-07 16:17:27 +0900

    入力マネージャを作成

--- GameCamp2023/Assets/Scripts/GameMain.cs
+++ GameCamp2023/Assets/Scripts/GameMain.cs
@@ -2,17 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameMain : MonoBehaviour
+namespace game
 {
-    // Start is called before the first frame update
-    void Start()
+    public class GameMain : MonoBehaviour
     {
-        
-    }
+        public const int PlayerNum = 3;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        public enum PlayerIndex
+        {
+            ZOU = 0,
+            WAKAME = 1,
+            UNI = 2
+        }
+
+        public PlayerInput inputManager;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            // 入力マネージャ　インスタンス生成と初期化
+            inputManager = new PlayerInput();
+            inputManager.Initialize();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            // 入力情報の更新
+            inputManager.UpdateInputStatus();
+
+            if (Input.GetKeyDown("space"))
+            {
+                inputManager.GetInputStatus(PlayerIndex.ZOU);
+            }
+        }
     }
 }
