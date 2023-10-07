dc66c09b15e2550168ebf9a2a96cfcbb4d24f22a
SeigaSouma <221120@st.yoshida-g.ac.jp>
2023-10-07 18:02:47 +0900

    PlayerZouスクリプト追加

---
+++ Ver_PlayerBase/GameCamp2023/Assets/Scripts/PlayerWakame.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerWakame : game.PlayerBase
+{
+    
+    // MonoBehaviourによる初期化処理
+    protected virtual void Start()
+    {
+        FireFlag = false;
+    }
+
+    // MonoBehaviourによる更新処理
+    protected virtual void Update()
+    {
+        if (FireFlag == true)
+        {
+            Fire();
+        }
+    }
+
+    // 移動処理 GameMainのUpdateから呼ばれる
+    public virtual void Move(game.PlayerInput.InputStatus inputStatus, Tilemap tileMap)
+    {
+        Vector3 MoveAxis = new Vector3();
+        MoveAxis = Vector3.zero;
+        if (inputStatus.UP == true)
+        {
+            MoveAxis.x += MoveSpeed;
+        }
+        if (inputStatus.DOWN == true)
+        {
+            MoveAxis.x -= MoveSpeed;
+        }
+        if (inputStatus.LEFT == true)
+        {
+            MoveAxis.y -= MoveSpeed;
+        }
+        if (inputStatus.RIGHT == true)
+        {
+            MoveAxis.x += MoveSpeed;
+        }
+        if (inputStatus.FIRE == true)
+        {
+            FireFlag = true;
+        }
+
+        Vector3Int cellPosition = tileMap.WorldToCell(transform.position);
+
+        //tileMap.SetTile(cellPosition, redTile);
+
+        transform.Translate(MoveAxis.x, MoveAxis.y, 0.0f, Space.Self);
+
+    }
+
+    protected virtual void Fire()
+    {
+        // 弾発射
+    }
+
+    protected virtual void OnCollisionEnter2D(Collision2D collision)
+    {
+        // タイルとの接触処理
+    }
+
+}
---
+++ Ver_PlayerBase/GameCamp2023/Assets/Test/TileColManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileColManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // É^ÉCÉãè„Ç…èÊÇ¡ÇƒÇ¢ÇÈîªíË
+    public void OnTile()
+    {
+        //if ()
+        {
+            GetComponent<Renderer>().material.color = Color.red;
+        }
+    }
+
+}
