fb1c35f8f8ab0e6ab199f13dc9329ee06fe43055
SeigaSouma <221120@st.yoshida-g.ac.jp>
2023-10-07 18:35:51 +0900

    壁追加 TileMapをprotectedに変更

--- GameCamp2023/Assets/Scripts/PlayerWakame.cs
+++ GameCamp2023/Assets/Scripts/PlayerWakame.cs
@@ -5,24 +5,25 @@ using UnityEngine.Tilemaps;
 
 public class PlayerWakame : game.PlayerBase
 {
+    //[]
+
     protected override void OnCollisionEnter2D(Collision2D collision)
     {
         // タイルとの接触処理
 
         // プレイヤーの位置をセル座標に変換
-        Vector3Int cellPosition =  .WorldToCell(transform.position);
-        collision.gameObject.GetComponent<Tilemap>.
+        Vector3Int cellPosition = tileMap.WorldToCell(transform.position);
 
         // 現在のタイル名取得
-        if (tilemap.GetTile(cellPosition) != null)
+        if (tileMap.GetTile(cellPosition) != null)
         {
-            string[] name = tilemap.GetTile(cellPosition).name.Split();
+            string[] name = tileMap.GetTile(cellPosition).name.Split();
 
             // セル座標に赤いタイルを設定
             if (name[0].Equals("Wall") == false &&
-        name[0].Equals("OuterWall") == false)
+                name[0].Equals("OuterWall") == false)
             {
-                tilemap.SetTile(cellPosition, redTile);
+                //tileMap.SetTile(cellPosition, redTile);
             }
         }
     }
