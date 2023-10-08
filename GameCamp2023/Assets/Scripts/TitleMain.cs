a5972507c1a63027ee753c071049aafc227d134e
SeigaSouma <221120@st.yoshida-g.ac.jp>
2023-10-08 13:28:32 +0900

    タイトルBGM、SE追加

--- GameCamp2023/Assets/Scripts/TitleMain.cs
+++ GameCamp2023/Assets/Scripts/TitleMain.cs
@@ -9,12 +9,22 @@ namespace title
     {
         private TitleInput inputManager;
 
+        [SerializeField]
+        private AudioClip se;
+
+        private AudioSource audioSource;
+
+        private float time = 0.0f;
+        private bool bEnter = false;
+
         // Start is called before the first frame update
         void Start()
         {
             // 入力マネージャ　インスタンス生成と初期化
             inputManager = new TitleInput();
             inputManager.Initialize();
+
+            audioSource = GetComponent<AudioSource>();
         }
 
         // Update is called once per frame
@@ -25,6 +35,18 @@ namespace title
 
             if (inputManager.GetInputStatus(0).RIGHT == true ||
                 Input.anyKeyDown)
+            {
+                audioSource.PlayOneShot(se);
+                bEnter = true;
+            }
+
+            if(bEnter == false)
+            {
+                return;
+            }
+
+            time += Time.deltaTime;
+            if(time >= 1.0f)
             {
                 SceneManager.LoadScene("MAPScene", LoadSceneMode.Single);
             }
